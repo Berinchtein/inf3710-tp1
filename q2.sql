52f1e8de8fcacc209fa272d4a73926c36bf4e33d
Manuel Jarry <lemanujarry@gmail.com>
2026-09-10 10:08:17 -0400

    docs(q2): compléter q2

--- q2.sql
+++ q2.sql
@@ -1,1 +1,11 @@
 -- Question 2 : Écrivez votre requête SQL ici
+-- Manuel Jarry 2375616, 10 septembre 2026
+
+SELECT
+       P.nom        AS 'nom_base',
+       E.methode    AS 'nom_evolution'
+
+  FROM POKEMON    P
+  JOIN EVOLUTIONS E ON E.id = P.id
+
+ WHERE P.type_principal = 'Feu';
