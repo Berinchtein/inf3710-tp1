50ff24514e1f901f901aff6e765c4af1b932e056
Manuel Jarry <lemanujarry@gmail.com>
2026-09-10 10:23:51 -0400

    fix(q2): ajuster nom_evolution correctement

--- q2.sql
+++ q2.sql
@@ -1,10 +1,11 @@
 -- Question 2 : Écrivez votre requête SQL ici
 -- Manuel Jarry 2375616, 10 septembre 2026
 
-SELECT PB.nom    AS 'nom_base',
-       E.methode AS 'nom_evolution'
+SELECT PB.nom AS 'nom_base',
+       PE.nom AS 'nom_evolution'
 
   FROM EVOLUTIONS E
   JOIN POKEMON    PB ON PB.id = E.pokemon_base_id
+  JOIN POKEMON    PE ON PE.id = E.pokemon_evolue_id
 
  WHERE PB.type_principal = 'Feu';
